1bf3d32431bac5d075f852408704b814bd0129a9
Steven Wake <steven@kyanmedia.com>
2009-01-20 14:16:48 +0000

    Removing / renaming old files

--- 0.1/classes/moojon.app.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class moojon_app extends moojon_base {
-	private $controller;
-	
-	protected function construct() {
-		
-	}
-}
-?>
--- 0.1/classes/moojon.controller.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-class moojon_controller extends moojon_base {
-	protected function construct() {
-		echo "****found me controller****<br />";		
-	}
-}
-?>
