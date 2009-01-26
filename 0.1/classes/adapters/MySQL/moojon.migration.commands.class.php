34fa02f6dd5cec8e24ffd26599c3082f4db27893
Steven Wake <steven@kyanmedia.com>
2009-01-26 20:13:51 +0000

    Migrtion command work

--- 0.1/classes/adapters/MySQL/moojon.migration.commands.class.php
+++ 0.1/classes/adapters/MySQL/moojon.migration.commands.class.php
@@ -3,8 +3,16 @@ final class moojon_migration_commands extends moojon_base {
 	private function __construct() {}
 	
 	static public function find_or_create_schema_migrations_table() {
+		$table_exists = false;
 		foreach (moojon_query::run_raw('SHOW TABLES;') as $table) {
-			echo $table['Tables_in_'.moojon_config::get_db()].'<br />';
+			if (in_array('schema_migrations', $table)) {
+				$table_exists = true;
+			}
+		}
+		if ($table_exists) {
+			die('has');
+		} else {
+			die('has not');
 		}
 	}
 }
