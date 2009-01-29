ed421453cea055f7be45f39808703ba4bf1d2e39
Steven Wake <steven@kyanmedia.com>
2009-01-29 22:28:40 +0000

    Adding stub methods to migration base class

--- 0.1/classes/adapters/MySQL/moojon.base.migration.class.php
+++ 0.1/classes/adapters/MySQL/moojon.base.migration.class.php
@@ -5,5 +5,23 @@ abstract class moojon_base_migration extends moojon_base {
 	abstract public function up();
 	
 	abstract public function down();
+	
+	final protected function create_table($name, $options) {}
+	
+	final protected function drop_table($name) {}
+	
+	final protected function rename_table($old_name, $new_name) {}
+	
+	final protected function create_table($name, $options) {}
+	
+	final protected function add_column($table_name, $column_name, $type, $options) {}
+	
+	final protected function remove_column($table_name, $column_name) {}
+	
+	final protected function rename_column($table_name, $old_column_name, $new_column_name) {}
+	
+	final protected function add_index($table_name, $column_name, $options) {}
+	
+	final protected function remove_index($table_name, $column_name) {}
 }
 ?>
