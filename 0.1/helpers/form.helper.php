cfbae5ea04618c8d9b2dc9aac0dde6191142b2a5
Steven Wake <stevenwake@steven-wakes-imac.local>
2009-02-18 23:26:09 +0000

    Date select options methods

--- 0.1/helpers/form.helper.php
+++ 0.1/helpers/form.helper.php
@@ -68,10 +68,10 @@ final class moojon_model_form extends moojon_form_tag {
 				$tag = new moojon_relationship_tag($column, $relationship->read(), $key);
 			}
 			if ($label) {
-				$fieldset->add_child(moojon_quick_tags::label(null, $column));
+				//$fieldset->add_child(moojon_quick_tags::label(null, $column));
 			}
 			if ($tag != null) {
-				$fieldset->add_child($tag);
+				//$fieldset->add_child($tag);
 			}
 		}
 		$this->add_child($fieldset);
@@ -80,6 +80,7 @@ final class moojon_model_form extends moojon_form_tag {
 		} else {
 			$submit_value = 'Update';
 		}
+		$this->add_child(moojon_quick_tags::datetime_selects(array('name' => 'publish_date')));
 		$this->add_child(new moojon_input_tag(array('name' => 'submit', 'value' => $submit_value, 'type' => 'submit')));
 		$this->add_child(new moojon_input_tag(array('name' => 'submit', 'value' => 'Cancel', 'type' => 'submit')));
 	}
@@ -134,6 +135,185 @@ final class moojon_quick_tags extends moojon_base {
 		$attributes['type'] = 'text';
 		$attributes['class'] = 'text';
 		return new moojon_input_tag($attributes);
+	}
+	
+	static public function datetime_selects($attributes = null, $format = null, $selected = null, $start = null, $end = null) {
+		if ($format == null) {
+			$format = 'Y/m/d G:i:s';
+		}
+		$return = '';
+		for ($i = 0; $i < strlen($format); $i ++) {
+			$select_attributes = $attributes;
+			$f = substr($format, $i, 1);
+			$select_attributes['name'] = $attributes['name']."_$f";
+			$select_attributes['id'] = $attributes['id']."_$f";
+			switch ($f) {
+				case 'Y':
+				case 'y':
+					$select = self::years_select_options($start, $end, $select_attributes, $selected, $f);
+					break;
+				case 'm':
+				case 'n':
+					$select = self::months_select_options($select_attributes, $selected, $f);
+					break;
+				case 'd':
+				case 'j':
+					$select = self::days_select_options($select_attributes, $selected, $f);
+					break;
+				case 'G':
+				case 'H':
+					$select = self::hours_select_options($select_attributes, $selected, $f);
+					break;
+				case 'i':
+					$select = self::minutes_select_options($select_attributes, $selected);
+					break;
+				case 's':
+					$select = self::seconds_select_options($select_attributes, $selected);
+					break;
+				default:
+					$select = $f;
+					break;
+			}
+			if (method_exists($select, 'render')) {
+				$return .= $select->render();
+			} else {
+				$return .= $select;
+			}
+		}
+		return $return;
+	}
+	
+	static public function seconds_select_options($attributes = null, $selected = null) {
+		$seconds = array();
+		for($i = 0; $i < 60; $i ++) {
+			if ($i < 10) {
+				$key = "0$i";
+			} else {
+				$key = $i;
+			}
+			$seconds[$key] = $i;
+		}
+		if ($selected == null) {
+			$selected = date('s');
+		}
+		return self::select_options($seconds, $selected, $attributes);
+	}
+	
+	static public function minutes_select_options($attributes = null, $selected = null) {
+		$minutes = array();
+		for($i = 0; $i < 60; $i ++) {
+			if ($i < 10) {
+				$key = "0$i";
+			} else {
+				$key = $i;
+			}
+			$minutes[$key] = $i;
+		}
+		if ($selected == null) {
+			$selected = date('i');
+		}
+		return self::select_options($minutes, $selected, $attributes);
+	}
+	
+	static public function hours_select_options($attributes = null, $selected = null, $format = null) {
+		if ($format != 'G' && $format != 'H') {
+			$format = 'G';
+		}
+		$hours = array();
+		for($i = 0; $i < 24; $i ++) {
+			if ($i < 10 && $format == 'G') {
+				$key = "0$i";
+			} else {
+				$key = $i;
+			}
+			$hours[$key] = $i;
+		}
+		if ($selected == null) {
+			$selected = date('G');
+		}
+		return self::select_options($hours, $selected, $attributes);
+	}
+	
+	static public function days_select_options($attributes = null, $selected = null, $format = null) {
+		if ($format != 'd' && $format != 'j') {
+			$format = 'd';
+		}
+		$days = array();
+		for($i = 1; $i < 31; $i ++) {
+			if ($i < 10 && $format == 'd') {
+				$key = "0$i";
+			} else {
+				$key = $i;
+			}
+			$days[$key] = $i;
+		}
+		if ($selected == null) {
+			$selected = date('j');
+		}
+		return self::select_options($days, $selected, $attributes);
+	}
+	
+	static public function months_select_options($attributes = null, $selected = null, $format = null) {
+		if ($format != 'm' && $format != 'n') {
+			$format = 'm';
+		}
+		$months = array();
+		for($i = 1; $i < 13; $i ++) {
+			if ($i < 10 && $format == 'm') {
+				$key = "0$i";
+			} else {
+				$key = $i;
+			}
+			$months[$key] = $i;
+		}
+		if ($selected == null) {
+			$selected = date('n');
+		}
+		return self::select_options($months, $selected, $attributes);
+	}
+	
+	static public function years_select_options($start = null, $end = null, $attributes = null, $selected = null, $format = null) {
+		if ($format != 'Y' && $format != 'y') {
+			$format = 'Y';
+		}
+		$years = array();
+		if ($start == null) {
+			$start = abs(date('Y') - 50);
+		}
+		if ($end == null) {
+			$end = abs(date('Y') + 50);
+		}
+		if ($selected == null) {
+			$selected = date('Y');
+		}
+		for($i = min($start, $end); $i < (max($start, $end) + 1); $i ++) {
+			$value = $i;
+			if ($format == 'y') {
+				$years[substr($value, -2)] = $value;
+			} else {
+				$years[$value] = $value;
+			}
+		}
+		return self::select_options($years, $selected, $attributes);
+	}
+	
+	static public function select_options($options, $selected = null, $attributes = null) {
+		return new moojon_select_tag(self::options($options, $selected), $attributes);
+	}
+	
+	static public function options($data, $selected = null) {
+		if (is_array($data) == false) {
+			$data = array($data);
+		}
+		$options = array();
+		foreach ($data as $key => $value) {
+			$attributes = array('value' => $value);
+			if ($value == $selected) {
+				$attributes['selected'] = 'selected';
+			}
+			$options[] = new moojon_option_tag($key, $attributes);
+		}
+		return $options;
 	}
 }
 
