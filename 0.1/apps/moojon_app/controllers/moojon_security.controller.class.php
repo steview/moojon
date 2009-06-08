f82cc70379763cb7d95344b6a8615b004c156234
Steven Wake <steven@kyanmedia.com>
2009-06-08 15:03:13 +0100

    Adding re-direct after loggin

--- 0.1/apps/moojon_app/controllers/moojon_security.controller.class.php
+++ 0.1/apps/moojon_app/controllers/moojon_security.controller.class.php
@@ -8,6 +8,7 @@ final class moojon_security_controller extends moojon_base_controller {
 		$this->security_password_key = moojon_config::get('security_password_key');
 		$this->security_remember_key = moojon_config::get('security_remember_key');
 		$this->security_key = moojon_config::get('security_key');
+		$security = '';
 		$security = $_REQUEST[$this->security_key];
 		if (is_array($security) == true && moojon_server::is_post() == true) {
 			$this->security_identity_value = $security[$this->security_identity_key];
@@ -20,6 +21,7 @@ final class moojon_security_controller extends moojon_base_controller {
 				$this->security_failure_message = sprintf(moojon_config::get('security_failure_message'), strtolower($this->security_identity_label), strtolower($this->security_password_label));
 			} else {
 				moojon_flash::set('notification', 'You have been logged in');
+				$this->forward(moojon_config::get('security_action'), moojon_config::get('security_controller'), moojon_uri::get_app());
 			}
 		}
 	}
