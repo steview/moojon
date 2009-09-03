fb29065f52d18bd342363089bb50f909c4bfe3bf
Steven Wake <steven@kyanmedia.com>
2009-09-03 17:04:43 +0100

    More REST URI function work

--- 0.1/helpers/url.helper.php
+++ 0.1/helpers/url.helper.php
@@ -9,52 +9,20 @@ function link_to($content, $action, $controller = null, $app = null, $attributes
 	return '<a href="'.moojon_config::key('index_file')."$app/$controller/$action\">$content</a>";
 }
 foreach (moojon_routes::get_rest_routes() as $route) {
-	$resource = $route->get_resource();
-	$singular = moojon_inflect::singularize($resource);
+	$collection = $route->get_resource();
+	$member = moojon_inflect::singularize($collection);
 	$id_property = $route->get_id_property();
-	
-	$resource_url = $resource.'_url';
-	$singular_url = $singular.'_url';
-
-	define($resource.'_url', create_function('', "return moojon_config::key('index_file').'$resource/';"));
-	define($singular.'_url', create_function('$model', "\$id_property = '$id_property';return feeds_url().\$model->$id_property.'/';"));
-	define('create_'.$singular_url, create_function('$model', "return feed_url(\$model).'create/';"));
-	define('update_'.$singular_url, create_function('$model', "return feed_url(\$model).'update/';"));
-	define('delete_'.$singular_url, create_function('$model', "return feed_url(\$model).'destroy/';"));
+	$collection_url = $collection.'_url';
+	$member_url = $member.'_url';
+	$create_url = 'create_'.$member_url;
+	$update_url = 'update_'.$member_url;
+	$delete_url = 'url_'.$member_url;
+	$$collection_url = create_function('', "return moojon_config::key('index_file').'$collection/';");
+	$$member_url = create_function('$model', "\$id_property = '$id_property';return $collection_url().\$model->$id_property.'/';");
+	$$create_url = create_function('', "return $$collection_url().'create/';");
+	$$update_url = create_function('$model', "return $member_url(\$model).'update/';");
+	$$delete_url = create_function('$model', "return $member_url(\$model).'destroy/';");
 }
-
-/*feeds_url
-feed_url $instance
-new_feed_url
-edit_feed_url $instance
-delete_feed_url $instance
-
-function feeds_url() {
-	return moojon_config::key('index_file').'feeds/';
-}
-
-function feed_url($model) {
-	$id_property = 'id';
-	return feeds_url().$model->$id_property.'/';
-}
-
-function create_feed_url($model) {
-	return feed_url($model).'create/';
-}
-
-function update_feed_url($model) {
-	return feed_url($model).'update/';
-}
-
-function destroy_feed_url($model) {
-	return feed_url($model).'destroy/';
-}
-
-
-feeds_url, '', "return moojon_config::key('index_file').'feeds/';"
-feed_url moojon_base_model, '$model', "$id_property = 'id';return feeds_url().$model->$id_property.'/';"
-create_feed_url, '$model', "return feed_url($model).'create/';"
-update_feed_url, '$model', "return feed_url($model).'update/';"
-destroy_feed_url, '$model', "return feed_url($model).'destroy/';"
-*/
+echo $firsts_url().'<br />';
+echo $create_first_url().'<br />';
 ?>
