993c35766a9537c74f6bada1ae63e5d75f9bac1a
System Administrator <root@steven-wakes-macbook-pro.local>
2009-09-03 20:46:15 +0100

    Work on scaffold methods

--- testing.dev/controllers/client/firsts.controller.class.php
+++ testing.dev/controllers/client/firsts.controller.class.php
@@ -5,32 +5,38 @@ final class firsts_controller extends moojon_base_controller {
 	}
 	
 	public function create() {
-		//$this->first = first::read(moojon_uri::get('id'));
+		$this->first = first::create();
+		if (moojon_server::is_post()) {
+			$this->first->update(moojon_uri::key('first'));
+			if ($this->first->save()) {
+				die('<p>Redirecting...</p>');
+			}
+		}
 	}
 	
 	public function read() {
-		$this->first = first::read(moojon_uri::get('id'));
+		$this->first = first::read(moojon_uri::key('id'));
 	}
 	
 	public function update() {
-		$this->first = first::read(moojon_uri::get('id'));
+		$this->first = first::read(moojon_uri::key('id'));
 	}
 	
 	public function destroy() {
-		$this->first = first::read(moojon_uri::get('id'));
+		$this->first = first::read(moojon_uri::key('id'));
 		if (moojon_server::is_delete()) {
 			$this->first->destroy();
 		}
 	}
 	
 	public function save() {
-		$this->first = first::read(moojon_uri::get('id'));
+		$this->first = first::create(moojon_uri::key('id'));
 		$this->first->update(moojon_post::key_or_null('first'));
 		if (!$this->first->save()) {
 			if (moojon_server::is_post()) {
 				$this->redirect('http://testing.dev:8888/index.php/create');
 			} else {
-				$this->redirect('http://testing.dev:8888/index.php/'.moojon_uri::get('id').'/edit');
+				$this->redirect('http://testing.dev:8888/index.php/'.moojon_uri::key('id').'/edit');
 			}
 		}
 	}
