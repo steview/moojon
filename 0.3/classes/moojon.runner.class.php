d332b02745816ed898b1b050cd8c4786140350ed
Steven Wake <stevenwake@steven-wakes-macbook-pro.local>
2010-01-23 16:29:49 +0000

    Debug 1

--- 0.3/classes/moojon.runner.class.php
+++ 0.3/classes/moojon.runner.class.php
@@ -14,8 +14,8 @@ final class moojon_runner extends moojon_singleton {
 		require_once(MOOJON_DIRECTORY.'/functions/moojon.core.functions.php');
 		switch (strtoupper(UI)) {
 			case 'CGI':
-				die('<>');
 				ini_set('memory_limit', '256M');
+				die('<>');
 				moojon_session::fetch();
 				moojon_uri::fetch();
 				$uri = moojon_uri::get_uri();
