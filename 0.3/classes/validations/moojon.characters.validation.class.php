63b6fa1ffabb9e0c9ff5d6c314c5f1e276af2842
Steven Wake <stevenwake@steven-wakes-macbook-pro.local>
2010-02-17 20:32:19 +0000

    Finishing character validation

--- 0.3/classes/validations/moojon.characters.validation.class.php
+++ 0.3/classes/validations/moojon.characters.validation.class.php
@@ -5,14 +5,21 @@ final class moojon_characters_validation extends moojon_base_validation {
 	
 	public function __construct($characters, $key, $message, $required = true) {
 		if (!is_array($characters)) {
-			$characters = explode('', $characters);
+			$characters = explode(', ', $characters);
+			for ($i = 0; $i < count($characters); $i ++) {
+				$characters[$i] = trim(substr($characters[$i], 0, 1));
+			}
 		}
 		$this->characters = $characters;
 		parent::__construct($key, $message, $required);
 	}
 	
 	public function valid($data) {
-		$characters = explode('', $data['data']);
+		$characters = array();
+		$data = $data['data'];
+		for ($i = 0; $i < strlen($data); $i ++) {
+			$characters[] = substr($data, $i, 1);
+		}
 		$return = true;
 		foreach ($characters as $character) {
 			if (!in_array($character, $this->$characters)) {
